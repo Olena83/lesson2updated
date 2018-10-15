6620d1dee501385af74f8d17d75f8f999027f83f
Olena83 <lvelga2007@gmail.com>
2018-10-16 01:03:38 +0300

    add updated

--- src/main/java/com/lits/Tasks2.java
+++ src/main/java/com/lits/Tasks2.java
@@ -1,8 +1,9 @@
 package com.lits;
 
 public class Tasks2 {
-    public static void main(String[] args) {
-        System.out.println("HERE");
-        System.out.println("HERE #2");
-    }
-}
+    public static void main(String[] args){
+               for (int a=1;a <=512; a *= 2)
+
+        {
+            System.out.println (a);}
+    }}
