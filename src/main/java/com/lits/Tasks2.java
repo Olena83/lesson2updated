684291dadfabdfd587ce887db0b8394944f55166
Olena83 <lvelga2007@gmail.com>
2018-10-18 22:52:58 +0300

    add updated

--- src/main/java/com/lits/Tasks2.java
+++ src/main/java/com/lits/Tasks2.java
@@ -2,8 +2,12 @@ package com.lits;
 
 public class Tasks2 {
     public static void main(String[] args){
-               for (int a=1;a <=512; a *= 2)
+              // for (int a=1;a <=512; a *= 2)
+        //{System.out.println (a);}
 
-        {
-            System.out.println (a);}
+//for (int a = 14; a>10; a=a+2)
+//if((a%7==0)&&(a%3!=0))
+        //{System.out.println (a);}
+
+
     }}
